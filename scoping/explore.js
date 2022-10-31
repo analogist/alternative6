cc099099d120995969d9eed4f8bf0268602a9093
James Wu <james@analogist.net>
2022-10-30 22:03:58 -0700

    add working search as filter

--- scoping/explore.js
+++ scoping/explore.js
@@ -1,21 +1,41 @@
 const { createApp } = Vue
 
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-}
-
 createApp({
   data() {
     return {
-      compplandata: null
+      compplandata: null,
+      compplanidx: null,
+      filterstr: "",
+      compplanfiltered: null
     };
   },
   created() {
     fetch('compplan-searchable-comments.json')
       .then(response => response.json())
       .then(data => (this.compplandata = data))
+    fetch('compplan-search-index.json')
+      .then(response => response.json())
+      .then(data => (
+        this.compplanidx = lunr.Index.load(data)
+        ))
+  },
+  methods: {
+    filterideas() {
+      if (this.filterstr == "") {
+        this.compplanfiltered = null;
+        return;
+      }
+      var outputstruct = this.compplanidx.search(this.filterstr);
+      var outputarr = outputstruct.map(el => el.ref);
+      this.compplanfiltered = this.compplandata.filter(el => outputarr.includes(el.slug));
+    }
+  },
+  computed: {
+    compplandisplay() {
+      return this.compplanfiltered == null ? this.compplandata : this.compplanfiltered
+    },
+    numfiltered() {
+      return this.compplanfiltered == null ? this.compplandata.length : this.compplanfiltered.length;
+    }
   }
 }).mount('#exploreapp')
